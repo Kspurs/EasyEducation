5e873734983e1a8cae70988e0ceee87faa8632f4
kspursoracle <1074847834.com>
2023-03-13 23:14:47 +0800

    test

--- src/pages/map.js
+++ src/pages/map.js
@@ -4,13 +4,10 @@ const Search =()=>{
     fetch("https:/api.map.baidu.com/place/v2/search?query=苏州相城区咖啡馆&output=json&ak=OO3OnnbVrc2PoWk7fOnyMutBvC4H58Fb",{mode:"no-cors",credentials:"omit"}).then(
             res => res.json()
     ).then(
-        responsedata => {
-            d=responsedata
-        }
+        responsedata => console.log(responsedata)
     )
     return(
         <p>
-            {d}
         </p>
     )
 }
