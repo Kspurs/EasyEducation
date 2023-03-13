4eba417e04060b13f05eca94b5c901286ace289c
kspursoracle <1074847834.com>
2023-03-13 23:18:31 +0800

    test

--- src/pages/map.js
+++ src/pages/map.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 const Search =()=>{
     let d;
     fetch("https:/api.map.baidu.com/place/v2/search?query=苏州相城区咖啡馆&output=json&ak=OO3OnnbVrc2PoWk7fOnyMutBvC4H58Fb",{mode:"no-cors",credentials:"omit"}).then(
-            res => res.json()
+            response => response.json()
     ).then(
         responsedata => console.log(responsedata)
     )
