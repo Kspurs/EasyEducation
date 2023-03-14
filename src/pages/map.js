9e46161d42462bd66d28a1ed4a2b1e36eaab9dd5
kspursoracle <1074847834.com>
2023-03-14 18:32:34 +0800

    test

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 const Search =()=>{
     let d;
-    fetch("https:/api.map.baidu.com/place/v2/search?query=苏州相城区咖啡馆&region=苏州&output=json&ak=78KGIdkPra58lY4dhfzmg1It7EOFo3Xq",{mode:"cors",credentials:"omit"}).then(
+    fetch("https://api.map.baidu.com/place/v2/search?query=苏州相城区咖啡馆&region=苏州&output=json&ak=78KGIdkPra58lY4dhfzmg1It7EOFo3Xq//GET请求").then(
             response => response.json()
     ).then(
         responsedata => console.log(responsedata)
